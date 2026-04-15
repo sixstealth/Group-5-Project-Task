8b4ec662862055b58cdf17132d433b72cf3fd4cb
sixstealth <shamandarkk@gmail.com>
2026-04-15 15:26:57 +0200

    added player stealth mechanic with visual feedback

--- PlayerVisibility.cs
+++ PlayerVisibility.cs
@@ -3,10 +3,32 @@ using UnityEngine;
 public class PlayerVisibility : MonoBehaviour
 {
     public bool IsHidden = false;
+    
+
+    private Renderer _renderer;
+    private Color _originalColor;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        if (_renderer != null)
+        {
+            _originalColor = _renderer.material.color;
+        }
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl)) IsHidden = true;
-        if (Input.GetKeyUp(KeyCode.LeftControl)) IsHidden = false;
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            IsHidden = true;
+            if (_renderer != null) _renderer.material.color = Color.gray; 
+        }
+        
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            IsHidden = false;
+            if (_renderer != null) _renderer.material.color = _originalColor; 
+        }
     }
 }
